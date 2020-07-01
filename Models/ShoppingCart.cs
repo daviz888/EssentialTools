e5ad01040e49881668d0dc2db0f8320f5fc2158c
Daffy Duck <ayos888@gmail.com>
2020-07-01 21:05:21 +0800

    adding new files

---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator calc;
+
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculatedProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }
+}
